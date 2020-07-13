370030d82c3b4b114bce3665a2b07b3193fd6ef7
Viktor Holovin <holovin@mail.com>
2020-07-13 22:28:34 +0200

    Add model post with djangorestframework

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from blog.serializers import PostSerializer
+from .models import Post
 
-# Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Post.objects.all().order_by('-created_on')
+    serializer_class = PostSerializer
